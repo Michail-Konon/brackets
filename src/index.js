2fea637e4d217040053f40e9c9782857242ac7db
michail-konon <greateyuri@gamil.com>
2023-02-07 21:15:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, brConfig) {
+	let strArr = str.split('');
+	for ( let i = 0; i < brConfig.length; i++ ) {
+		for ( let j = 0; j < strArr.length; j++ ) {
+			if ( brConfig[i][0] == strArr[j] && strArr[j + 1] == brConfig[i][1] ) {
+				strArr.splice(j, 2);
+				i = 0;	
+				j = -1;
+			}
+		}
+	}
+  return strArr.length != 0 ? false : true
 }
